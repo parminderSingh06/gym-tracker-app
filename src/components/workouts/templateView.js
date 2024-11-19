9ce8d785ebd99afd967d6334249d4f4a45e4f203
Parminder Singh <parmindersingh1086@gmail.com>
2024-11-18 23:19:32 -0500

    Added templates to the workout page

--- src/components/workouts/templateView.js
+++ src/components/workouts/templateView.js
@@ -5,7 +5,15 @@ import '../../styles/TemplateView.css';
 function TemplateView(){
     return (
         <div id='template-section'>
-            <ul id='template-grid'>
+            <p><strong>My Templates</strong></p>
+            <ul className='template-grid'>
+                <li><TemplateItem/></li>
+                <li><TemplateItem/></li>
+                <li><TemplateItem/></li>
+                <li><TemplateItem/></li>
+            </ul>
+            <p><strong>Example Templates</strong></p>
+            <ul className='template-grid'>
                 <li><TemplateItem/></li>
                 <li><TemplateItem/></li>
                 <li><TemplateItem/></li>
